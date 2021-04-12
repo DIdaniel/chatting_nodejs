41753220cfcdbf90bbe553dbd6e3fb765a561500
dream_Faunus <cappu159@gmail.com>
2021-04-12 22:41:25 +0900

    completed ALL

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const app = express();
 const path = require('path');
 const server = http.createServer(app);
 const socketIO = require('socket.io');
-
 const io = socketIO(server);
+const moment = require('moment');
 
 
 
@@ -15,7 +15,12 @@ const PORT = process.env.PORT || 5000;
 
 io.on("connection", (socket) => {
   socket.on("chatting", (data) => {
-    io.emit("chatting", data)
+    const {name, msg} = data;
+    io.emit("chatting", {
+      name,
+      msg,
+      time: moment(new Date()).format("h:ss A")
+    })
   })
 })
 
--- src/js/chat.js
+++ src/js/chat.js
@@ -6,6 +6,7 @@ const nickName = document.querySelector('#nickName');
 const chatList = document.querySelector('.chatting-list');
 const chatInput = document.querySelector('.chatting-input');
 const sendButton = document.querySelector('.send-button');
+const displayContainer = document.querySelector('.display-container');
 
 sendButton.addEventListener('click', () => {
   const param = {
@@ -17,9 +18,28 @@ sendButton.addEventListener('click', () => {
 })
 
 socket.on("chatting", (data) => {
-  const li = document.createElement('li');
-  li.innerText = `${data.name}님이 - ${data.msg}`;
-  chatList.appendChild(li);
+  console.log(data);
+  const {name, msg, time} = data;
+  const item = new LiModel(name, msg, time); // LiModel의 instance화
+  item.makeLi()
+  displayContainer.scrollTo(0, displayContainer.scrollHeight)
 })
 
-console.log(socket);
+function LiModel(name, msg, time) {
+  this.name = name;
+  this.msg = msg;
+  this.time = time;
+
+  this.makeLi = () => {
+    const li = document.createElement('li');
+    li.classList.add(nickName.value === this.name ? "sent" : "received")
+    const dom = `<span class="profile">
+            <span class="user">${this.name}</span>
+            <img src="https://placeimg.com/50/50/any" alt="any">
+          </span>
+          <span class="message">${this.msg}</span>
+          <span class="time">${this.time}</span>`;
+    li.innerHTML = dom;
+    chatList.appendChild(li);
+  }
+}
